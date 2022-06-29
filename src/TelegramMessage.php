5dc8d6b4cc754ceb2ff6ceabe0d08e8f4a17c3cb
Alexander Opekunov <iam@opekunov.com>
2022-06-29 21:12:33 +0300

    Update TelegramMessage.php

--- src/TelegramMessage.php
+++ src/TelegramMessage.php
@@ -283,6 +283,52 @@ class TelegramMessage extends Telegram
     }
 
     /**
+     * Send answer callback query
+     *
+     * https://core.telegram.org/bots/api#answercallbackquery
+     *
+     * @param  string  $callbackQueryId  Unique identifier for the query to be answered
+     * @param  string|null  $text  Text of the notification. If not specified, nothing will be shown to the user, 0-200 characters
+     * @param  bool  $showAlert  If True, an alert will be shown by the client instead of a notification at the top of the chat screen. Defaults to
+     *     false.
+     * @param  string|null  $url  URL that will be opened by the user's client. If you have created a Game and accepted the conditions via BotFather,
+     * specify the URL that opens your game - note that this will only work if the query comes from a callback_game button. Otherwise, you may use
+     *     links like t.me/your_bot?start=XXXX that open your bot with a parameter.
+     * @param  int  $cacheTime  The maximum amount of time in seconds that the result of the callback query may be cached client-side. Telegram apps
+     *     will support caching starting in version 3.14. Defaults to 0.
+     *
+     * @return array
+     * @throws Exceptions\TelegramRequestException
+     * @throws Exceptions\TelegramTooManyRequestsException|TelegramException
+     */
+    public function sendAnswerCallbackQuery(
+        string $callbackQueryId,
+        string $text = '',
+        bool $showAlert = false,
+        string $url = '',
+        int $cacheTime = 0
+    ): array {
+        if (mb_strlen($text > 100)) {
+            throw new TelegramException('Text of the notification need between 0-200 characters, '.mb_strlen($text > 100).' passed');
+        }
+
+        $payload = [
+            'callback_query_id' => $callbackQueryId,
+            'show_alert'        => $showAlert,
+            'cache_time'        => $cacheTime
+        ];
+        if (!empty($text)) {
+            $payload['text'] = $text;
+        }
+
+        if (!empty($url)) {
+            $payload['url'] = $url;
+        }
+
+        return $this->sendRequest('answerCallbackQuery', $payload);
+    }
+
+    /**
      * Set reply keyboard
      *
      * @param  array<array>  $rows  Rows of buttons (array of arrays)
